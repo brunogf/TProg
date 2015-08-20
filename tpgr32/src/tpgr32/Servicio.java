ed140ed574a027541bd05fb9e70f522f07d36e52
piñe <pie@JuanDalto>
2015-08-20 13:52:26 -0300

    Seas

---
+++ tpgr32/src/tpgr32/Servicio.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tpgr32;
+
+/**
+ *
+ * @author piñe
+ */
+public class Servicio {
+    
+    private String descripcion_;
+    private String[] imagenes_;
+    private float precio_;
+    
+    
+    public Servicio(){
+        
+    }
+    
+    public String getDescripcion(){
+        
+        return descripcion_;
+    }
+    
+    public String[] getImagenes(){
+        
+        return imagenes_;
+    }
+    
+    
+    public float getPrecio(){
+        
+        return precio_;
+    } 
+    
+    
+    
+}
