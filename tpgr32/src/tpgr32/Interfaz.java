03238090dcce3abdc9baf931a7bba1c2b38d56e2
Nicolas Rostan <nicolas.rostan@fing.edu.uy>
2015-08-19 21:19:50 -0300

    Agregue Internal Frame de Reg Usuario y Barras de Menu

--- tpgr32/src/tpgr32/Interfaz.java
+++ tpgr32/src/tpgr32/Interfaz.java
@@ -16,6 +16,11 @@ public class Interfaz extends javax.swing.JFrame {
      */
     public Interfaz() {
 	initComponents();
+	setLocationRelativeTo(null); //Centra el MainFrame en la pantalla
+	PanelCentral.removeAll();
+	PanelCentral.repaint();
+	PanelCentral.revalidate(); //HACE QUE NO SE VEA NADA AL ABRIR EL PROGRAMA
+	
     }
 
     /**
@@ -27,21 +32,151 @@ public class Interfaz extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        PanelCentral = new javax.swing.JPanel();
+        RegistrarUsuarioFrame = new javax.swing.JInternalFrame();
+        jLabel1 = new javax.swing.JLabel();
+        RegUsuarioNicknameLabel = new javax.swing.JLabel();
+        RegUsuarioNombreLabel = new javax.swing.JLabel();
+        RegUsuarioApellidoLabel = new javax.swing.JLabel();
+        RegUsuarioCorreoLabel = new javax.swing.JLabel();
+        RegUsuarioFechaLabel = new javax.swing.JLabel();
+        RegUsuarioSeleccionarImagenButton = new javax.swing.JButton();
+        BarraMenu = new javax.swing.JMenuBar();
+        MenuInicio = new javax.swing.JMenu();
+        MenuRegistros = new javax.swing.JMenu();
+        MenuRegistrarUsuario = new javax.swing.JMenuItem();
+        MenuConsultas = new javax.swing.JMenu();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Tarea 1");
+        setLocation(new java.awt.Point(0, 0));
+        setPreferredSize(new java.awt.Dimension(450, 550));
+        setResizable(false);
+        setSize(new java.awt.Dimension(450, 550));
+
+        PanelCentral.setLayout(new java.awt.CardLayout());
+
+        RegistrarUsuarioFrame.setVisible(false);
+
+        jLabel1.setText("Registrar Usuario");
+
+        RegUsuarioNicknameLabel.setText("Nickname:");
+
+        RegUsuarioNombreLabel.setText("Nombre:");
+
+        RegUsuarioApellidoLabel.setText("Apellido:");
+
+        RegUsuarioCorreoLabel.setText("Correo:");
+
+        RegUsuarioFechaLabel.setText("Fecha de Nacimiento:");
+
+        RegUsuarioSeleccionarImagenButton.setText("Seleccionar Imagen");
+        RegUsuarioSeleccionarImagenButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                RegUsuarioSeleccionarImagenButtonActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout RegistrarUsuarioFrameLayout = new javax.swing.GroupLayout(RegistrarUsuarioFrame.getContentPane());
+        RegistrarUsuarioFrame.getContentPane().setLayout(RegistrarUsuarioFrameLayout);
+        RegistrarUsuarioFrameLayout.setHorizontalGroup(
+            RegistrarUsuarioFrameLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(RegistrarUsuarioFrameLayout.createSequentialGroup()
+                .addGroup(RegistrarUsuarioFrameLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(RegistrarUsuarioFrameLayout.createSequentialGroup()
+                        .addGap(151, 151, 151)
+                        .addComponent(jLabel1))
+                    .addGroup(RegistrarUsuarioFrameLayout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(RegUsuarioNombreLabel))
+                    .addGroup(RegistrarUsuarioFrameLayout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(RegUsuarioNicknameLabel))
+                    .addGroup(RegistrarUsuarioFrameLayout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(RegUsuarioApellidoLabel))
+                    .addGroup(RegistrarUsuarioFrameLayout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(RegUsuarioCorreoLabel))
+                    .addGroup(RegistrarUsuarioFrameLayout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(RegUsuarioFechaLabel))
+                    .addGroup(RegistrarUsuarioFrameLayout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(RegUsuarioSeleccionarImagenButton)))
+                .addContainerGap(152, Short.MAX_VALUE))
+        );
+        RegistrarUsuarioFrameLayout.setVerticalGroup(
+            RegistrarUsuarioFrameLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(RegistrarUsuarioFrameLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 14, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(RegUsuarioNicknameLabel)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(RegUsuarioNombreLabel)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(RegUsuarioApellidoLabel)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(RegUsuarioCorreoLabel)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(RegUsuarioFechaLabel)
+                .addGap(37, 37, 37)
+                .addComponent(RegUsuarioSeleccionarImagenButton)
+                .addContainerGap(203, Short.MAX_VALUE))
+        );
+
+        PanelCentral.add(RegistrarUsuarioFrame, "card2");
+
+        MenuInicio.setText("Inicio");
+        BarraMenu.add(MenuInicio);
+
+        MenuRegistros.setText("Registros");
+
+        MenuRegistrarUsuario.setText("Registrar Usuario");
+        MenuRegistrarUsuario.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                MenuRegistrarUsuarioActionPerformed(evt);
+            }
+        });
+        MenuRegistros.add(MenuRegistrarUsuario);
+
+        BarraMenu.add(MenuRegistros);
+
+        MenuConsultas.setText("Consultas");
+        BarraMenu.add(MenuConsultas);
+
+        setJMenuBar(BarraMenu);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(PanelCentral, javax.swing.GroupLayout.DEFAULT_SIZE, 402, Short.MAX_VALUE)
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(PanelCentral, javax.swing.GroupLayout.DEFAULT_SIZE, 443, Short.MAX_VALUE)
+                .addContainerGap())
         );
+
+        getAccessibleContext().setAccessibleDescription("");
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void MenuRegistrarUsuarioActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_MenuRegistrarUsuarioActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_MenuRegistrarUsuarioActionPerformed
+
+    private void RegUsuarioSeleccionarImagenButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_RegUsuarioSeleccionarImagenButtonActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_RegUsuarioSeleccionarImagenButtonActionPerformed
 
     /**
      * @param args the command line arguments
@@ -83,5 +218,19 @@ public class Interfaz extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JMenuBar BarraMenu;
+    private javax.swing.JMenu MenuConsultas;
+    private javax.swing.JMenu MenuInicio;
+    private javax.swing.JMenuItem MenuRegistrarUsuario;
+    private javax.swing.JMenu MenuRegistros;
+    private javax.swing.JPanel PanelCentral;
+    private javax.swing.JLabel RegUsuarioApellidoLabel;
+    private javax.swing.JLabel RegUsuarioCorreoLabel;
+    private javax.swing.JLabel RegUsuarioFechaLabel;
+    private javax.swing.JLabel RegUsuarioNicknameLabel;
+    private javax.swing.JLabel RegUsuarioNombreLabel;
+    private javax.swing.JButton RegUsuarioSeleccionarImagenButton;
+    private javax.swing.JInternalFrame RegistrarUsuarioFrame;
+    private javax.swing.JLabel jLabel1;
     // End of variables declaration//GEN-END:variables
 }
--- tpgr32/src/tpgr32/Usuario.java
+++ tpgr32/src/tpgr32/Usuario.java
@@ -52,5 +52,6 @@ public class Usuario {
         this.correoElec = ce;
     }
     
-    
+ 
+   
 }
