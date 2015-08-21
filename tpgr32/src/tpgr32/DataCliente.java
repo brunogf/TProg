8110ef778c8f4de79413b68d6eb05dcf850e6296
Nicolas Rostan <nicolas.rostan@fing.edu.uy>
2015-08-20 22:37:13 -0300

    DataUsuario, Cliente, proveedor

---
+++ tpgr32/src/tpgr32/DataCliente.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tpgr32;
+import java.awt.Image;
+import java.util.*;
+/**
+ *
+ * @author Nicolás Rostán
+ */
+public class DataCliente extends DataUsuario {
+    private Set<DataReserva> reservas_;
+    public DataCliente(){}
+    public DataCliente(String nickname, String nombre, String apellido, String correo, Date fecha)
+    {
+	super(nickname, nombre, apellido, correo, fecha);
+	reservas_ = new HashSet<>();
+    }
+    public DataCliente(String nickname, String nombre, String apellido, String correo, Date fecha, Image img)
+    {
+	super(nickname, nombre, apellido, correo, fecha,img);
+    }
+    
+    public Set<DataReserva> getReservas()
+    {
+	return reservas_;
+    }
+    
+    public void agergarReserva(DataReserva dr)
+    {
+	reservas_.add(dr);
+    }
+    
+    
+    
+}
---
+++ tpgr32/src/tpgr32/DataUsuario.java
@@ -0,0 +1,96 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tpgr32;
+import java.awt.Image;
+import java.util.Date;
+
+/**
+ *
+ * @author Nicolás Rostán
+ */
+public abstract class DataUsuario {
+    private String nickname_;
+    private String nombre_;
+    private String apellido_;
+    private String correo_;
+    private Date fecha_;
+    private Image img_;
+    
+    public DataUsuario(){}
+    public DataUsuario(String nickname, String nombre, String apellido, String correo, Date fecha)
+    {
+	nickname_ = nickname;
+	nombre_ = nombre;
+	apellido_ = apellido;
+	correo_ = correo;
+	fecha_ = fecha;
+    }
+    public DataUsuario(String nickname, String nombre, String apellido, String correo, Date fecha, Image img)
+    {
+	this(nickname, nombre, apellido, correo, fecha);//llama constructor anterior
+	img_ = img;	
+    }
+    
+    public String getNickname()
+    {
+	return nickname_;
+    }
+    
+    public String getNombre()
+    {
+	return nombre_;
+    }
+    
+    public String getApellido()
+    {
+	return apellido_;
+    }
+    
+    public String getCorreo()
+    {
+	return correo_;
+    }
+    
+    public Date getFecha()
+    {
+	return fecha_;
+    }
+    
+    public Image getImage()
+    {
+	return img_;
+    }
+    
+    public void setNickname(String nick)
+    {
+	nickname_ = nick;
+    }
+    
+    public void setNombre(String nombre)
+    {
+	nombre_ = nombre;
+    }
+    
+    public void setApellido(String ap)
+    {
+	apellido_ = ap;
+    }
+    
+    public void setCorreo(String c)
+    {
+	correo_ = c;
+    }
+    
+    public void setFecha(Date f)
+    {
+	fecha_ = f;
+    }
+    
+    public void setImage(Image i)
+    {
+	img_ = i;
+    }
+}
