61095aadb1997895eece984f8e8634eed505ce9e
piñe <pie@JuanDalto>
2015-08-22 14:42:18 -0300

    cree la clase vacia

---
+++ tpgr32/src/tpgr32/ControladorReserva.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tpgr32;
+
+/**
+ *
+ * @author piñe
+ */
+public class ControladorReserva {
+    
+    public ControladorReserva(){
+        
+    }
+    
+}
