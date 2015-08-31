87c8641640bb66086198e804ac9e4c9b70d8d936
piñe <pie@JuanDalto>
2015-08-31 00:53:04 -0300

    Agregue operaciones listarpaises y listarciudades en Ictrpublicacion

--- tpgr32/src/tpgr32/IControladorPublicacion.java
+++ tpgr32/src/tpgr32/IControladorPublicacion.java
@@ -7,6 +7,7 @@ package tpgr32;
 
 import java.util.Set;
 import java.awt.Image;
+import javax.swing.tree.DefaultTreeModel;
 /**
  *
  * @author spesamosca
@@ -18,10 +19,12 @@ public interface IControladorPublicacion {
     void altaPromocion(String nombre, Set<DataServicio> servicios, float descuento);
     void altaServicio(String nombre, String descripcion, Set<Image> img, float precio,
         Set <String> categorias, String proveedor, DataUbicacion origen, DataUbicacion destino);
+    void altaServicio(String nombre, String descripcion, Set<Image> imagenes, float precio, Set<String>categorias,
+                          String proveedor, DataUbicacion origen);
     void eliminarCategoriaServicio(String cat);
     DataPromocion infoPromocion(String proveedor, String promo);
     DataServicio infoServicio(String proveedor, String servicio);
-    TreeCategoria listarCategorias();
+    DefaultTreeModel listarCategorias();
     Set<DataPromocion> listarPromociones();
     Set<DataServicio> listarServicios();
     Set<DataServicio> listarServiciosDeCategoria(String cat);
@@ -29,7 +32,9 @@ public interface IControladorPublicacion {
     void modificarImagenesServicio(Set<Image> imgs);
     void modificarOrigenServicio(String origen);
     void modificarPrecioServicio(float precio);
+    void registrarCategoria(String nombre);
     void registrarCategoria(String nombre, String padre);
     void seleccionarServicio(String proveedor, String nombre);    
-
+    Set<String> listarPaises();
+    Set<String> listarCiudades(String pais);
 }
